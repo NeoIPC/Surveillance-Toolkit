4c5f86660414d376df51e0865fbaba755ee8ab66
Brar Piening <brar.piening@charite.de>
2025-03-01 11:24:27 +0100

    Validation report as published in the first e-mail distribution (#6)
    Add the NeoIPC Surveillance Validation report as it was sent to partners in the first e-mail distribution.

---
+++ reports/Validation Report/build-reports.ps1
@@ -0,0 +1,64 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Position=0)]
+    [System.String]
+    $SiteCodeFilter = '.+',
+
+    [Parameter(Position=1)]
+    [System.String]
+    $Language = 'en'
+)
+
+$token = Get-Content -LiteralPath "$PSScriptRoot/../../../token.txt" -Raw -Encoding utf8NoBOM
+
+$depts = (curl -sH "Authorization: ApiToken $token" `
+'https://neoipc.charite.de/api/organisationUnitGroups.json?paging=false&filter=code:eq:NEO_DEPARTMENT&fields=organisationUnits%5Bcode%5D' |
+ConvertFrom-Json -Depth 10).organisationUnitGroups[0].organisationUnits.code | Where-Object -FilterScript { $_ -match $SiteCodeFilter } | Sort-Object
+
+$wd = Get-Location
+Set-Location -LiteralPath $PSScriptRoot
+
+foreach ($site in $depts) {
+    Write-Host "Generating validation report for $site..."
+    $outFile = "$([datetime]::Now.ToString('yyyy-MM-dd_HHmmss'))_NeoIPC-Surveillance-Validation-Report_$($site).$($Language).pdf"
+    $skipRest = $false
+    $errorLine = ''
+    $isError = $false
+    quarto render --profile $Language -P "language:$Language" -P "token:../../../token.txt" -P "departmentFilter:$($site)" -o $outFile 2>&1 | ForEach-Object -Process {
+        if ($skipRest) {
+            return
+        }
+        $s = "$_"
+        if ($s -eq 'System.Management.Automation.RemoteException') {
+            $s = ''
+        }
+        if ($isError) {
+            if ($s -eq '! No problem detected') {
+                Write-Host "No problem detected." -ForegroundColor DarkYellow
+                $skipRest = $true
+            }
+            else {
+                if ($errorLine.Length -gt 0) {
+                    Write-Error -Message $errorLine
+                    $errorLine = ''
+                }
+                Write-Error -Message $s
+            }
+        }
+        elseif ($s -match '^(Error)|(Fehler)') {
+            $isError = $true
+            $errorLine = $s
+        }
+        elseif ($s -match "^(`e\[39m)?(`e\[33m)?WARNING") {
+            $s | Write-Warning
+        }
+        else {
+            $s | Write-Verbose
+        }
+    }
+    if (-not $skipRest -and -not $isError) {
+        Write-Host "done." -ForegroundColor Green
+    }
+}
+
+Set-Location -LiteralPath $wd
